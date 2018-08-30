9177fc85a62e2b2fecaeb92a25a218e539fd7ef6
ChrisChrisLoLo <sporewoh@gmail.com>
2018-08-29 20:22:22 -0700

    Refactored code to be less fragmented.

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # local import
 from app.config import app_config
@@ -9,16 +10,24 @@ from app.config import app_config
 # initialize sql-alchemy
 db = SQLAlchemy()
 
-def create_app(config_name):
-    app = Flask(__name__)
-    api = Api(app)
-    app.config.from_object(app_config[config_name])
 
-    # TODO: App.config.from_x doesn't seem to work currently so configs are hard coded in.
-    # This should be figured out and fix when possible
-    #app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-    #app.config['DEBUG'] = True 
+def create_app():
+    """
+    Initialize the Flask app, the SQLAlchemy db, and the Flask-Restful Api
+    Configurates the app based on the app.config, which is selected from a .env file.
+    Sets up migration commands for the flask cli
+    """
+    app = Flask(__name__)
+
+    app.config.from_object(app_config[os.getenv('APP_SETTINGS')])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db = SQLAlchemy()
     db.init_app(app)
+
+    api = Api(app)
+
+    # Create migration commands
+    migrate = Migrate(app,db)
 
     return app, api
--- run.py
+++ run.py
@@ -1,27 +1,13 @@
 import os
-from flask import Flask
-from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
+from app import create_app
 from app.config import app_config
 from controllers import courseController
 
 
 ### INITIALIZATION
-# Initialize the Flask app, the SQLAlchemy db, and the Flask-Restful Api
-# Configurates the app based on the app.config, which is selected from a .env file.
-# Sets up migration commands for the flask cli
-app = Flask(__name__)
-app.config.from_object(app_config[os.getenv('APP_SETTINGS')])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy()
-db.init_app(app)
-
-api = Api(app)
-
-migrate = Migrate(app,db)
+# Initialize the flask app,api,db, and any commands
+app,api = create_app()
 
 ### ROUTING
 # The routing will occur in run.py for now. A new routing file may need to
