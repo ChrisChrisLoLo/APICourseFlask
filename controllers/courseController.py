e747c2dc142e907cb9145a948fc3cbe5c56ab29f
ChrisChrisLoLo <sporewoh@gmail.com>
2018-09-02 13:01:15 -0700

    Added query parameter validation.

--- controllers/courseController.py
+++ controllers/courseController.py
@@ -1,5 +1,5 @@
 from models.courseModel import * 
-from flask import jsonify
+from flask import jsonify, request
 
 course_schema = CourseSchema()
 courses_schema = CourseSchema(many=True)
@@ -12,8 +12,24 @@ def routeCourses(app):
     #api.add_resource(CourseResource, '/courses')
     @app.route('/courses',methods=['GET'])
     def getCourses():
+        VALID_QUERY_PARAMETERS = ('minCourseNum','maxCourseNum','subject','faculty','sortBy','sortOrder')
+        for argument in request.args:
+            print(argument)
+            if argument not in VALID_QUERY_PARAMETERS:
+                print('ERROR')
+                return errorResponse(400,'Parameter \'{}\' is not a valid query parameter for the requested URL.'.format(argument))
         courses = Course.query.limit(5).all()
-        print(courses)
+        #print(request.args)
         courses = courses_schema.dump(courses).data
         res = jsonify({'status': 200,'data': courses})
         return res, 200
+
+#TODO: Make this function a decorator and utilize it if need be.
+def validateQueryParameters(parameterSet):
+    for argument in request.args:
+        if argument not in parameterSet:
+            return errorResponse(400,'Parameter \'{}\' is not a valid query parameter for the requested URL.'.format(argument))
+
+def errorResponse(HTTPStatus,message):
+    res = jsonify({'status':HTTPStatus,'message': message})
+    return res, HTTPStatus
